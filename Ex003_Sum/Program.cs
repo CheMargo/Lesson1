aad74fe0210acc88964da3e84f51597435432087
Margo <ritosnsk@gmail.com>
2022-09-18 14:05:09 +0600

    add changes

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 3;
-int numB = 7;
+﻿int numA = new Random(). Next(1, 10);
+int numB = new Random(). Next(1, 10);
+Console.WriteLine(numA);
+Console.WriteLine(numB);
 int Sum = numA + numB;
 Console.Write(Sum);
