498dc256b48819901be298b4da97a23bd8be49c7
Margo <ritosnsk@gmail.com>
2022-09-18 13:57:53 +0600

    more Examples

---
+++ Ex003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numA = 3;
+int numB = 7;
+int Sum = numA + numB;
+Console.Write(Sum);
---
+++ Ex004_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿double numA = 10;
+double numB = 3;
+double result = (numA/numB);
+Console.WriteLine(result);
