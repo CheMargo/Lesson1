f7d3abee8efb88c9bf2161ce7c7ffabe07a12425
Margo <ritosnsk@gmail.com>
2022-09-18 15:39:31 +0600

    add Max and Magic

---
+++ Ex006_Max/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 9;
+int c = 6;
+int d = 3;
+int e = 8;
+
+int Max = a;
+
+if (a>Max) Max = a;
+if (b>Max) Max = b;
+if (c>Max) Max = c;
+if (d>Max) Max = d;
+if (e>Max) Max = e;
+Console.Write("Максимальное число - ");
+Console.WriteLine(Max);
---
+++ Ex007_Magic/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("X");
+
+int xa = 50; 
+int ya = 1;
+int xb = 1; 
+int yb = 30;
+int xc = 100; 
+int yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("X");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("X");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("X");
+
+int x = xa, y = xb;
+int count = 0;
+
+while (count < 5000)
+{
+    int cur = new Random().Next(0,3);
+    if (cur == 0)
+    {
+        x=(x+xa)/2;
+        y=(y+ya)/2;
+    }
+    
+    if (cur == 1)
+    {
+        x=(x+xb)/2;
+        y=(y+yb)/2;
+    }
+    
+    if (cur == 2)
+    {
+        x=(x+xc)/2;
+        y=(y+yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("X");
+    count++;
+}
+    
