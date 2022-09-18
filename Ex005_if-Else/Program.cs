afbf2cae367c2555e1c7cee5e43f3734ed1ddc6b
Margo <ritosnsk@gmail.com>
2022-09-18 14:23:29 +0600

    If-Else

---
+++ Ex005_if-Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите ваше имя!");
+string name = Console.ReadLine();
+if (name.ToLower() == "рита")
+{
+    Console.WriteLine("Гантер так ждал тебя, Рита");
+}
+else
+{
+    Console.Write("Опять ты - ");
+    Console.WriteLine(name);
+}
